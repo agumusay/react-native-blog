c59cb09deabc536ee79a0543112969d4ccbbb57e
Ali San Gumusay <silvermoon@pop-os.localdomain>
2020-06-21 18:39:03 +0200

    Blog Context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,18 +1,64 @@
 import createDataContext from './createDataContext';
+import jsonServer from '../api/jsonServer';
 
 const blogReducer = (state, action) => {
 	switch (action.type) {
 		case 'add_blogpost':
-			return [...state, { title: `Blog Post #${state.length + 1}` }];
+			return [
+				...state,
+				{
+					id: Math.floor(Date.now() + Math.random()),
+					title: action.payload.title,
+					content: action.payload.content,
+				},
+			];
+		case 'delete_blogpost':
+			return state.filter(
+				blogPost => blogPost.id !== action.payload
+			);
+		case 'edit_blogpost':
+			return state.map(blogPost =>
+				blogPost.id === action.payload.id
+					? action.payload
+					: blogPost
+			);
+
 		default:
 			return state;
 	}
 };
 
 const addBlogPost = dispatch => {
-	return () => {
-		dispatch({ type: 'add_blogpost' });
+	return (title, content, callBack) => {
+		dispatch({
+			type: 'add_blogpost',
+			payload: { title, content },
+		});
+		callBack && callBack();
 	};
 };
 
-export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost }, []);
+const deleteBlogPost = dispatch => {
+	return id => {
+		dispatch({ type: 'delete_blogpost', payload: id });
+	};
+};
+
+const editBlogPost = dispatch => {
+	return (id, title, content, callback) => {
+		dispatch({
+			type: 'edit_blogpost',
+			payload: { id, title, content },
+		});
+		callback && callback();
+	};
+};
+
+export const {
+	Context,
+	Provider,
+} = createDataContext(
+	blogReducer,
+	{ addBlogPost, deleteBlogPost, editBlogPost },
+	[{ title: 'TEST POST', content: 'TEST CONTENT', id: 1 }]
+);
